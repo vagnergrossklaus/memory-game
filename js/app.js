d1b0a81f12def1b33af4ced4c4443a1eb064d8f4
vagnergros <vagnergrossklaus@gmail.com>
2019-02-28 20:17:59 -0300

    feat: Change card style class on click to show image

--- js/app.js
+++ js/app.js
@@ -2,8 +2,22 @@ const CARD_PAIR = 8;
 
 var cards = [];
 
+function showCard(card){
+    card.setAttribute('class', 'card show open');
+}
+
+function onClick(event) {
+    if (event.target.nodeName === 'LI') {
+        showCard(event.target);
+    }
+}
+
 function init() {
+
     cards = createCards(CARD_PAIR);
     shuffleCards(cards)
     createDeck(cards);
+
+    document.getElementById('deck').addEventListener('click', onClick);
+
 }
